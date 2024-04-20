7b8076a68608bcdc9667fa9987ebc603ee22f12a
Robbie West <robbie.west@ventriks.com>
2024-04-20 02:42:47 +0100

    Triangle

--- gltut/GLError.h
+++ gltut/GLError.h
@@ -1,21 +1,44 @@
 #pragma once
+#include "spdlog/spdlog.h"
+#include "GL/glew.h"
 
+//Miscellaneous
+
+template<typename... Args>
+std::string format_g(std::string s, Args... params) {
+	return std::vformat(s, std::make_format_args(params...));
+}
+/*
+template<typename ex, typename ...Args>
+void throwError(std::string message, Args... params) {
+	std::string out = format_g(message, params...);
+	spdlog::error(out);
+	throw ex(out);
+}
+*/
+
+//Debugging
 #define STOP(x) if(!(x)) __debugbreak();
 
 //Macro to wrap all gl calls
 #define glc(x) GLClearError();\
 	x;\
-	STOP(GLLogCall(#x, __FILE__, __LINE__))
+	STOP(GLLogCall(#x, __FILE__, __LINE__));
 
-void GLClearError();
-bool GLLogCall(const char* function, const char* file, int line);
+void GLClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
 
-void assert_cond(bool cond, std::string message);
+bool GLLogCall(const char* function, const char* file, int line) {
+	GLenum error;
+	while ((error = glGetError()) != GL_NO_ERROR) {
+		spdlog::error("Line {} {}\n| OpenGL ERROR | : [{}] {}", line, file, error, function);
+	}
+	return true;
+}
 
-
-//Miscellaneous
-template<typename... Args>
-std::string format(std::string s, Args... params);
-
-template<typename ex, typename ...Args>
-void throwError(std::string message, Args... params);
+void assert_cond(bool cond, std::string message) {
+	if (!cond) 
+		spdlog::error("Assertion error |{}| -> cond was false", message);
+	
+}
--- gltut/helper.h
+++ gltut/helper.h
@@ -26,8 +26,8 @@ void init() {
 		throw std::invalid_argument("glfw could not be initialized.");
 
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
